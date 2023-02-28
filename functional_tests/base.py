a553acf4654a4e9a1b407d82f5ef2c161c56371a
Arthur LAUNOY <alaunoy@consultants-solutec.fr>
2023-02-28 15:14:46 +0100

    New FT for sharing, move seesion creation stuff to base

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -4,6 +4,12 @@ from selenium.common.exceptions import WebDriverException
 import time
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.common.keys import Keys
+from django.contrib.auth import BACKEND_SESSION_KEY, SESSION_KEY
+from django.contrib.sessions.backends.db import SessionStore
+from django.conf import settings
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 MAX_WAIT = 10
 
@@ -63,3 +69,17 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.get_item_input_box().send_keys(Keys.ENTER)
         item_number = num_rows + 1
         self.wait_for_row_in_list_table(f'{item_number}: {item_text}')
+    
+    def create_pre_authenticated_session(self, email):
+        user = User.objects.create(email=email)
+        session = SessionStore()
+        session[SESSION_KEY]=user.pk
+        session[BACKEND_SESSION_KEY] = settings.AUTHENTICATION_BACKENDS[0]
+        session.save()
+
+        self.browser.get(self.live_server_url + '/404_no_such_url/')
+        self.browser.add_cookie(dict(
+            name=settings.SESSION_COOKIE_NAME,
+            value=session.session_key,
+            path='/'
+        ))
